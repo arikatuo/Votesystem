e79a26a5c3fc8cc68647a17f85115b2f5059811b
qinfen23863 <hspcadmin@WINDOWS-EPAVJTQ.hs.handsome.com.cn>
2018-10-23 16:08:17 +0800

    添加log4j日志系统

---
+++ src/log4j/Log4jTest.java
@@ -0,0 +1,22 @@
+package log4j;
+
+import org.apache.log4j.Logger;
+
+public class Log4jTest{
+
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getLogger(Log4jTest.class);
+
+        //logger使用的方法:
+        //1.直接打印字符串
+        logger.info("this is an info");
+        logger.warn("this is a warn");
+        logger.error("this is an error");
+        logger.fatal("this is a fatal");
+
+        //2.字符串和变量混合使用
+        String user = "Tom";
+        logger.info("当前登录用户user为" + user + "!" );
+    }
+}
