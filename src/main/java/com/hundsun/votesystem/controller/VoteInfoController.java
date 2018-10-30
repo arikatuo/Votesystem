dd3705203ed9c750d4e143ae3481448de38d7d55
sumimasah <qaz865915659>
2018-10-30 16:55:56 +0800

    修复当没有选择投票人会出现错误。

--- src/main/java/com/hundsun/votesystem/controller/VoteInfoController.java
+++ src/main/java/com/hundsun/votesystem/controller/VoteInfoController.java
@@ -21,48 +21,50 @@ import java.util.List;
 @RequestMapping("voteinfo")
 public class VoteInfoController {
 
-    @Autowired
-    private VoteInfoService voteInfoService;
+	@Autowired
+	private VoteInfoService voteInfoService;
 
-    //通过voteInfoId获取投票详情
-    @RequestMapping("finalvoteinfo")
-    public String test(HttpServletRequest request, HttpServletResponse response){
-        VoteUtils.kuayuSolution(request,response);
-        int voteInfoId= Integer.parseInt(request.getParameter("voteInfoId"));
-        HashMap<String,Object> totalResult= null;
-        HashMap<String,Integer> voterNum=new HashMap<>();
-        ReturnData returnData=new ReturnData();
-        Integer state = 1;
-        try {
-            totalResult = new HashMap<>();
-            VoteInfo voteInfo=voteInfoService.selectByPrimaryKey(voteInfoId);
-            List<HashMap<String,Integer>>  num=voteInfoService.getVoterNum(voteInfoId);
-            List<HashMap<String,Integer>>  optionNum=voteInfoService.getVoteOptionNum(voteInfoId);
-            if (num.size()>1){
-				voterNum.put("hasvote",num.get(0).get("voterNum"));
-				voterNum.put("novote",num.get(1).get("voterNum"));
-			}else {
-            	if (state.equals(num.get(0).get("voteState"))){
-					voterNum.put("hasvote",num.get(0).get("voterNum"));
-					voterNum.put("nosvote",0);
-				}else{
-					voterNum.put("novote",num.get(0).get("voterNum"));
-					voterNum.put("hasvote",0);
+	//通过voteInfoId获取投票详情
+	@RequestMapping("finalvoteinfo")
+	public String test(HttpServletRequest request, HttpServletResponse response) {
+		VoteUtils.kuayuSolution(request, response);
+		int voteInfoId = Integer.parseInt(request.getParameter("voteInfoId"));
+		HashMap<String, Object> totalResult = null;
+		HashMap<String, Integer> voterNum = new HashMap<>();
+		ReturnData returnData = new ReturnData();
+		Integer state = 1;
+		try {
+			totalResult = new HashMap<>();
+			VoteInfo voteInfo = voteInfoService.selectByPrimaryKey(voteInfoId);
+			List<HashMap<String, Integer>> num = voteInfoService.getVoterNum(voteInfoId);
+			List<HashMap<String, Integer>> optionNum = voteInfoService.getVoteOptionNum(voteInfoId);
+			if (num.size() == 2) {
+				voterNum.put("hasvote", num.get(0).get("voterNum"));
+				voterNum.put("novote", num.get(1).get("voterNum"));
+			} else if (num.size() == 1) {
+				if (state.equals(num.get(0).get("voteState"))) {
+					voterNum.put("hasvote", num.get(0).get("voterNum"));
+					voterNum.put("nosvote", 0);
+				} else {
+					voterNum.put("novote", num.get(0).get("voterNum"));
+					voterNum.put("hasvote", 0);
 				}
+			}else {
+				voterNum.put("hasvote", 0);
+				voterNum.put("nosvote", 0);
 			}
 
-            totalResult.put("voteInfo",voteInfo);
-            totalResult.put("voternum",voterNum);
-            totalResult.put("optionNum",optionNum);
-            returnData.setReturnObject(totalResult);
-        } catch (Exception e) {
-            returnData.setReturnMsg("error");
-            returnData.setReturnMsgDetail(e.getMessage());
-          
-        }
-        return new Gson().toJson(returnData);
-    }
-    
-    
+			totalResult.put("voteInfo", voteInfo);
+			totalResult.put("voternum", voterNum);
+			totalResult.put("optionNum", optionNum);
+			returnData.setReturnObject(totalResult);
+		} catch (Exception e) {
+			returnData.setReturnMsg("error");
+			returnData.setReturnMsgDetail(e.getMessage());
+
+		}
+		return new Gson().toJson(returnData);
+	}
+
 
 }
